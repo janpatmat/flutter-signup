64e261cc31aabb814b3499682a1fd54d2698a0da
Patrick <patmatayabas@gmail.com>
2023-01-17 16:18:13 +0800

    added assets in login form

--- lib/signupPage.dart
+++ lib/signupPage.dart
@@ -5,30 +5,39 @@ class signupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentWidth = MediaQuery.of(context).size.width *.5;
+    final currentWidth = MediaQuery.of(context).size.width * .5;
     final lightPurple = Color.fromRGBO(92, 25, 169, 100);
     return Scaffold(
-      
-        body:Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              color: lightPurple,
-            ),
-            Container(
-              height: 700,
-              width: currentWidth,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-            )
-          ],
-          
-            
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            color: lightPurple,
           ),
-         
-        );
-    
+          Container(
+            height: 600,
+            width: currentWidth,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 50,
+                    left: 0,
+                    right: 0,
+                    child: Image(
+                      image: AssetImage('images/personlogo.png'),
+                      height: 150,
+                      width: 150,
+                    ))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
